43716ce242fd0d4099d0864aee5eeda4e26c995d
Paul Tan <pyokagan@gmail.com>
2017-01-11 11:44:23 +0800

    controller/error: make error page more informative

--- controller/error.js
+++ controller/error.js
@@ -2,18 +2,22 @@
 /**
  * Error handler
  */
+const http = require('http');
 
 
 function formatErrorPage(e) {
+  const status = e.status || 500;
+  const title = status + ' ' + http.STATUS_CODES[status];
   const out = [];
   out.push('<!DOCTYPE html>');
   out.push('<html>');
   out.push('<head>');
-  out.push('<title>Fatal error</title>');
+  out.push('<title>' + title + '</title>');
   out.push('</head>');
   out.push('<body>');
-  out.push('<h1>Fatal error</h1>');
-  out.push('<pre>' + e.message + '</pre>');
+  out.push('<h1>' + title + '</h1>');
+  if (e.message)
+    out.push('<pre>' + e.message + '</pre>');
   out.push('</body>');
   out.push('</html>');
   return out.join('\n');
